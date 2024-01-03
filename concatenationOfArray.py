8f5650a9fc5bdcd27e167e96dded9bbbe6e312c1
netrunner2 <aodeyale@choosefreedomit.com>
2024-01-02 22:07:09 -0500

    Made small change to solution

--- concatenationOfArray.py
+++ concatenationOfArray.py
@@ -5,14 +5,10 @@ nums = [1, 3, 2, 1]
 
 class Solution:
     def getConcatenation(self, nums: [int]) -> [int]:
-        length = len(nums)
         ans = []
-        x = 0
 
-        while x < 2:
-            for i in range(length):
-                ans.append(nums[i])
-
-            x += 1
+        for i in range(2):
+            for n in nums:
+                ans.append(n)
 
         return ans
